6cc34fa7958ddd74039be343a2278692ce5ce5c7
unknown <merguibi>
2023-09-09 20:04:05 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header/Header'
+import About from './components/About/About'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
