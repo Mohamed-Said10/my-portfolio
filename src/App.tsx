414e28405b1f63b09aa25103837959fd10b11141
unknown <merguibi>
2023-10-03 02:13:29 +0100

    Navbar + Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css'
-import Header from './components/Header/Header'
-import About from './components/About/About'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Works from './components/Works/Works';
+import './App.css';
+import Navbar from './components/Layout/Navbar';
+
 
 function App() {
 
   return (
     <>
+    <Navbar/>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/works" element={<Works/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
