157c0cbeeb1447a69c5de0a0ee462da2693c27d1
unknown <merguibi>
2023-10-08 21:35:28 +0100

    My Latest Works

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import ProjectCarousel from "./ProjectCarousel";
 
-const Works: React.FC = () => {
+const Works = () => {
   return (
-    <></>
+    <>
+      <div className="bg-orange-200 h-screen flex  justify-center">
+          <div className="">
+            <ProjectCarousel />
+          </div>
+      </div>
+    </>
   );
 };
 
